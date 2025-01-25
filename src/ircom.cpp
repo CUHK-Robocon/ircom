1e9788b33166b4d9eee59b297d6e2e37eed82e1d
Yau-ming Leung <ymleung314@gmail.com>
2025-01-26 00:28:45 +0800

    Add logging to high-level API

--- src/ircom.cpp
+++ src/ircom.cpp
@@ -13,23 +13,32 @@ server::server(const char* service_name) : publisher_(service_name) {
 
 void server::handle_thread_f() {
   publisher_.publish();
+  spdlog::info("Service published");
 
   while (true) {
     boost::asio::ip::tcp::socket socket(io_ctx_);
     acceptor_.accept(socket);
+
+    boost::asio::ip::tcp::endpoint remote_endpoint = socket.remote_endpoint();
+    spdlog::info("New connection from {}:{}",
+                 remote_endpoint.address().to_string(), remote_endpoint.port());
   }
 }
 
 void client::connect() {
   while (true) {
+    spdlog::info("Discovering services");
     const discovery::service_info& info = browser_.get_latest_service();
+    spdlog::info("Selected service @ {}", info.addr);
 
     try {
       boost::asio::ip::tcp::resolver::results_type endpoints =
           resolver_.resolve(info.addr, SERVICE_PORT_STR);
 
+      spdlog::info("Connecting to service");
       boost::asio::ip::tcp::socket socket(io_ctx_);
       boost::asio::connect(socket, endpoints);
+      spdlog::info("Connected to service");
 
       break;
     } catch (boost::system::system_error err) {
