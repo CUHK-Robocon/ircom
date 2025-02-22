7f049f5b76cf1b0736fce04dd612220489a8b3b4
Yau-ming Leung <ymleung314@gmail.com>
2025-02-22 18:23:06 +0800

    Use async functions to allow shutdown

--- include/ircom/ircom.h
+++ include/ircom/ircom.h
@@ -19,38 +19,53 @@ namespace ircom {
 class server {
  public:
   explicit server(const char* service_name);
+  ~server();
 
   server(const server&) = delete;
   server& operator=(const server&) = delete;
 
  private:
-  void handle_thread_f();
+  boost::asio::awaitable<void> handle_conn();
+  boost::asio::awaitable<void> handler();
+  void io_ctx_thread_f();
 
+  // IMPORTANT: The IO context MUST BE ran from one thread only, required for
+  // graceful shutdown to work. Use a strand if multiple threads are needed.
   boost::asio::io_context io_ctx_;
   boost::asio::ip::tcp::acceptor acceptor_{
       io_ctx_,
       boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), SERVICE_PORT)};
-
-  std::thread handle_thread_;
+  boost::asio::ip::tcp::socket sock_{io_ctx_};
+  std::thread io_ctx_thread_;
 
   discovery::publisher publisher_;
+
+  bool shutdown_issued_ = false;
 };
 
 class client {
  public:
-  explicit client(const char* target_service_name)
-      : browser_(target_service_name) {};
+  explicit client(const char* target_service_name);
+  ~client();
 
   client(const client&) = delete;
   client& operator=(const client&) = delete;
 
-  void connect();
-
  private:
+  boost::asio::awaitable<void> handle_conn();
+  boost::asio::awaitable<void> connect();
+  void io_ctx_thread_f();
+
   discovery::browser browser_;
 
+  // IMPORTANT: The IO context MUST BE ran from one thread only, required for
+  // graceful shutdown to work. Use a strand if multiple threads are needed.
   boost::asio::io_context io_ctx_;
   boost::asio::ip::tcp::resolver resolver_{io_ctx_};
+  boost::asio::ip::tcp::socket sock_{io_ctx_};
+  std::thread io_ctx_thread_;
+
+  bool shutdown_issued_ = false;
 };
 
 }  // namespace ircom
--- src/ircom.cpp
+++ src/ircom.cpp
@@ -8,77 +8,177 @@
 namespace ircom {
 
 server::server(const char* service_name) : publisher_(service_name) {
-  handle_thread_ = std::thread(&server::handle_thread_f, this);
+  io_ctx_thread_ = std::thread(&server::io_ctx_thread_f, this);
 }
 
-void server_handle_conn(boost::asio::ip::tcp::socket& sock) {
-  while (true) {
-    // TODO: Exchange packet.
+server::~server() {
+  boost::asio::post(io_ctx_, [&]() {
+    sock_.close();
+    acceptor_.close();
+    shutdown_issued_ = true;
+  });
+  io_ctx_thread_.join();
+}
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+boost::asio::awaitable<void> server::handle_conn() {
+  while (true) {
+    // TODO: Read and process packet.
+
+    boost::asio::steady_timer timer(io_ctx_, boost::asio::chrono::seconds(1));
+    co_await timer.async_wait(boost::asio::use_awaitable);
   }
 }
 
-void server::handle_thread_f() {
+boost::asio::awaitable<void> server::handler() {
+  // Indeed, connections can reach the backlogs after `acceptor_` is opened.
+  // Placed here instead of in the constructor just to minimize the time between
+  // publication and service being actually available.
   publisher_.publish();
   spdlog::info("Service published");
 
-  while (true) {
-    try {
-      boost::asio::ip::tcp::socket sock(io_ctx_);
-      acceptor_.accept(sock);
+  try {
+    // Check against shutdown before the first iteration (e.g. when the
+    // destrctor is called before the start of the IO context thread).
+    while (!shutdown_issued_) {
+      try {
+        co_await acceptor_.async_accept(sock_, boost::asio::use_awaitable);
+      } catch (const boost::system::system_error& err) {
+        if (err.code() == boost::asio::error::operation_aborted) {
+          spdlog::info("Acceptor shut down");
+          break;
+        }
+        throw;
+      }
 
-      boost::asio::ip::tcp::endpoint remote_endpoint = sock.remote_endpoint();
+      boost::asio::ip::tcp::endpoint remote_endpoint = sock_.remote_endpoint();
       spdlog::info("New connection from {}:{}",
                    remote_endpoint.address().to_string(),
                    remote_endpoint.port());
 
-      server_handle_conn(sock);
-    } catch (boost::system::system_error err) {
-      spdlog::error(
-          "An error occurred for the connection, discarding connection: {}",
-          err.what());
-      continue;
+      try {
+        co_await handle_conn();
+      } catch (const boost::system::system_error& err) {
+        if (err.code() == boost::asio::error::operation_aborted ||
+            err.code() == boost::asio::error::eof && shutdown_issued_) {
+          spdlog::info("Ongoing communication shut down");
+          break;
+        }
+
+        spdlog::error(
+            "An error occurred for the connection, discarding connection: {}",
+            err.what());
+        sock_.close();
+        continue;
+      }
     }
+  } catch (const std::exception& err) {
+    spdlog::critical("An error is uncaught in the server handler: {}",
+                     err.what());
+    io_ctx_.stop();
+    co_return;
   }
 }
 
-void client_handle_conn(boost::asio::ip::tcp::socket& sock) {
+void server::io_ctx_thread_f() {
+  boost::asio::co_spawn(io_ctx_, handler(), boost::asio::detached);
+  io_ctx_.run();
+}
+
+client::client(const char* target_service_name)
+    : browser_(target_service_name) {
+  io_ctx_thread_ = std::thread(&client::io_ctx_thread_f, this);
+}
+
+client::~client() {
+  browser_.close();
+  boost::asio::post(io_ctx_, [&]() {
+    resolver_.cancel();
+    sock_.close();
+    shutdown_issued_ = true;
+  });
+  io_ctx_thread_.join();
+}
+
+boost::asio::awaitable<void> client::handle_conn() {
   while (true) {
-    // TODO: Exchange packet.
+    // TODO: Read and process packet.
+
+    boost::asio::steady_timer timer(io_ctx_, boost::asio::chrono::seconds(1));
+    co_await timer.async_wait(boost::asio::use_awaitable);
   }
 }
 
-void client::connect() {
-  while (true) {
-    spdlog::info("Discovering services");
-    const discovery::service_info& info = browser_.get_latest_service();
-    spdlog::info("Selected service @ {}", info.addr);
+boost::asio::awaitable<void> client::connect() {
+  try {
+    // Check against shutdown before the first iteration (e.g. when the
+    // destrctor is called before the start of the IO context thread).
+    while (!shutdown_issued_) {
+      spdlog::info("Discovering services");
+      discovery::service_info info;
+      try {
+        // TODO: Use an async version of `get_latest_service`.
+        info = browser_.get_latest_service();
+      } catch (const discovery::closed_exception&) {
+        spdlog::info("Service discovery stopped");
+        break;
+      }
+      spdlog::info("Selected service @ {}", info.addr);
 
-    boost::asio::ip::tcp::socket sock(io_ctx_);
-    try {
-      boost::asio::ip::tcp::resolver::results_type endpoints =
-          resolver_.resolve(info.addr, SERVICE_PORT_STR);
+      bool should_retry = false;
+      try {
+        boost::asio::ip::tcp::resolver::results_type endpoints =
+            co_await resolver_.async_resolve(info.addr, SERVICE_PORT_STR,
+                                             boost::asio::use_awaitable);
 
-      spdlog::info("Connecting to service");
-      boost::asio::connect(sock, endpoints);
-      spdlog::info("Connected to service");
-    } catch (boost::system::system_error err) {
-      spdlog::warn("Failed to connect to remote, will retry: {}", err.what());
-      // Retry cooldown.
-      std::this_thread::sleep_for(std::chrono::seconds(1));
-      continue;
+        spdlog::info("Connecting to service");
+        co_await boost::asio::async_connect(sock_, endpoints,
+                                            boost::asio::use_awaitable);
+        spdlog::info("Connected to service");
+      } catch (const boost::system::system_error& err) {
+        if (err.code() == boost::asio::error::operation_aborted) {
+          spdlog::info("Connection attempt cancelled");
+          break;
+        }
+
+        spdlog::warn("Failed to connect to remote, will retry: {}", err.what());
+        should_retry = true;
+      }
+      if (should_retry) {
+        // Retry cooldown.
+        boost::asio::steady_timer timer(io_ctx_,
+                                        boost::asio::chrono::seconds(1));
+        co_await timer.async_wait(boost::asio::use_awaitable);
+
+        continue;
+      }
+
+      try {
+        co_await handle_conn();
+      } catch (const boost::system::system_error& err) {
+        if (err.code() == boost::asio::error::operation_aborted ||
+            err.code() == boost::asio::error::eof && shutdown_issued_) {
+          spdlog::info("Ongoing communication shut down");
+          break;
+        }
+
+        spdlog::error(
+            "An error occurred for the connection, discarding connection: {}",
+            err.what());
+        sock_.close();
+        continue;
+      }
     }
-
-    try {
-      client_handle_conn(sock);
-    } catch (boost::system::system_error err) {
-      spdlog::error(
-          "An error occurred for the connection, discarding connection: {}",
-          err.what());
-      continue;
-    }
+  } catch (const std::exception& err) {
+    spdlog::critical("An error is uncaught in the client connection loop: {}",
+                     err.what());
+    io_ctx_.stop();
+    co_return;
   }
+}
+
+void client::io_ctx_thread_f() {
+  boost::asio::co_spawn(io_ctx_, connect(), boost::asio::detached);
+  io_ctx_.run();
 }
 
 }  // namespace ircom
